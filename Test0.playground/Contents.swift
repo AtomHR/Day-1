f1aeba7e5702fd6e8ea241212c5db8bf502e6145
AtomHR-ios <70606733+AtomHR@users.noreply.github.com>
2025-01-31 00:42:59 +0300

    Add files via upload

---
+++ Test0.playground/Contents.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+var greeting = "Hello, playground11232"
+
+var mane = "2517628"
+mane = "I neme is cat"
+let qwe = "rhqhfakjwe"
+let character = "Daphne"
+/*character = "Eloise" - let is constant
+character = "Francesca"*/
+print(greeting, qwe)
+var favoriteShow = "Or\"a\"nge is\"ds\" the New Black"
+favoriteShow = "The Good Place"
+favoriteShow = "Doctor \"Who"
+var qwee = 123
+var one = 1
+one = 2
+var asd = "qwdfghuio"
+print(one)
+let two = 2
+//let zxc = "hfhhhf"
+//zxc = "hfhfhfhf"
+var qaz = 12
+// var qaz = 1331
+
+var qweq = """
+weuoie
+qioweuioqwue
+wpoiqeipeqwo  \"rqewmlqr;wmrlqw;
+qewiqwe
+"""
+print(qweq.count)
+let ww = qweq.count
+print(ww)
+var qq = "QqWwEeRrTtYyUyIeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+let qq2 = qq.uppercased()
+var qw1 = "qqwwee"
+let qw2 = qw1.hasPrefix("qqw") // начинается ли строка с определенной буквы или предложения
+
+var qa1 = "qsdfew"
+let qa2 = qa1.hasSuffix("w") //заканчивается ли строка с опред... важен регистр поиска
+let qw3 = 100000000
+let qw4 = 100_000_000_000
+var q1 = qw3 - 2
+var q2 = qw3 + 2
+var q3 = qw3 * 2
+var q4 = qw3 / 2
+var q5 = qw3 * qw3
+
+var qqq = 1_0
+qqq += 10
+qqq -= 10
+qqq *= 10
+qqq /= 10
+
+let num = 10
+var num1 = num.isMultiple(of: 4) //является ли число х кратное числу х
+var num2 = 10.isMultiple(of: 5) // обращение прямо к определенному числу
+var uu = 12
+//uu = "j"
+let mu1 = 0.1 + 0.2
+var q12 = 1 + 1.0
+var q0 = 1
+var q01 = 1.0443
+// var c = q0 + q01
+var c = q0 + Int(q01)
+var c11 = Double(q0) + q01
+q01 *= 5
